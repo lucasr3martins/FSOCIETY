8b1cb860c0d362adfae69046d02495c9e6f20f8b
Lucas R <lucasr3martins@gmail.com>
2020-09-30 18:33:16 -0300

    init

---
+++ FSOCIETY/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View style={{flex: 1,justifyContent: "center",
+    alignItems: "center",backgroundColor: '#9400D3',}}>
+      <Text style={{color: 'white'}}>Eu estou dentro da FSOCIETY</Text>
+    </View>
+  )
+}
+
+
+export default HelloWorldApp;
